555c9b5b8514f734e5d2dc12bd60edea07a49c01
Breno <brenosousacst@gmail.com>
2025-09-24 13:37:26 -0300

    delete whatsapp messages routes

--- routes/api.php
+++ routes/api.php
@@ -29,9 +29,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('auth/login', [AuthController::class, 'login']);
 
-Route::get('/webhook/whatsapp', [WhatsappWebhookController::class, 'get']);
-Route::post('/webhook/whatsapp', [WhatsappWebhookController::class, 'handle']);
-
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('auth/me', [AuthController::class, 'me']);
     Route::post('auth/logout', [AuthController::class, 'logout']);
